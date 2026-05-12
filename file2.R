c412fcec5066aa709c5fd63128a746cfa4e51fd1
Sudhir Tamang <sudhirtamang5@gmail.com>
2026-05-12 12:55:04 -0400

    file2.R added

---
+++ file2.R
@@ -0,0 +1,7 @@
+# ==============================================================================
+# PROJECT:  NS-Estimator
+# SCRIPT:   file2.R
+# PURPOSE:  BIAS correction on the NS-Estimator
+# AUTHOR:   Sudhir T
+# DATE:     12th May, 2026 
+# ==============================================================================
