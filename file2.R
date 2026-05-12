12fa834350ec3200ad8e34eb623af0064b2c5068
Sudhir Tamang <sudhirtamang5@gmail.com>
2026-05-12 14:21:51 -0400

    Second edit firl2.R

--- file2.R
+++ file2.R
@@ -2,6 +2,20 @@
 # PROJECT:  NS-Estimator
 # SCRIPT:   file2.R
 # PURPOSE:  BIAS correction on the NS-Estimator
-# AUTHOR:   Sudhir T
+# AUTHOR:   Sudhir T.
 # DATE:     12th May, 2026 
 # ==============================================================================
+
+rm(list = ls())
+library(Tlasso)
+library(tensr)
+library(glasso)
+library(expm)
+library(rTensor)
+library(doParallel)
+
+source("C:/Users//sudhi//Desktop//Fast and Separable Estimation Replication//replication//Model 1//Separate.fit.R")
+source("C://Users//sudhi//Desktop//Fast and Separable Estimation Replication//replication//Model 1//simulation.summary.R")
+source("C://Users//sudhi//Desktop//Fast and Separable Estimation Replication//replication//Model 1//Model7.R")
+# source("C:/Users/sudhi/Desktop/PhD projects/NS-Estimator/Lfunctions.R")
+source("Lfunctions.R")
