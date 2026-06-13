d87cdf50a8f54a357f334c92874caaab860d4848
Sudhir Tamang <sudhirtamang5@gmail.com>
2026-06-13 10:25:57 -0400

    Almost finalized the compare file compareSigma

--- compareSigma.R
+++ compareSigma.R
@@ -37,31 +37,67 @@ dimen <- c(45, 57)
 K <- length(dimen)
 
 
-# plan(multisession, workers = ceiling(availableCores() * .7))
-# RHOs <- seq(-1, 1, 0.01)
-# Grho <- vector("double", length(RHOs))
-# B <- 10000
-# # func1 <- function(n, rho){
-# #   tmp1 <- mvtnorm::rmvnorm(n, mean=c(0, 0), sigma=matrix(c(1, rho, rho, 1), nrow=2))
-# #   mean(qnorm((n/(n+1)) * stats::ecdf(tmp1[, 1])(tmp1[, 1]))   * qnorm((n/(n+1)) * stats::ecdf(tmp1[, 2])(tmp1[, 2]))  )
-# # }
+plan(multisession, workers = ceiling(availableCores() * .7))
+RHOs <- seq(-1, 1, 0.01)
+Grho <- vector("double", length(RHOs))
+B <- 10000
 # func1 <- function(n, rho){
 #   tmp1 <- mvtnorm::rmvnorm(n, mean=c(0, 0), sigma=matrix(c(1, rho, rho, 1), nrow=2))
-#   mean(qnorm((1/(n+1)) * rank(tmp1[, 1]))   * qnorm((1/(n+1)) * rank(tmp1[, 2]))  )
+#   mean(qnorm((n/(n+1)) * stats::ecdf(tmp1[, 1])(tmp1[, 1]))   * qnorm((n/(n+1)) * stats::ecdf(tmp1[, 2])(tmp1[, 2]))  )
 # }
-# func2 <- function(rho, B, func1, n){
-#   results <- future_map_dbl(
-#     1:B, 
-#     \(idx) func1(rho = rho, n = n), # Only 'idx' is iterated
-#     .options = furrr_options(seed = 123)
-#   )
-#   mean(results)
-# }
-# Grho <- future_map_dbl(RHOs, func2, B = B, func1 = func1, n = 50, .options = furrr_options(seed = 123))
-# 
+func1 <- function(n, rho){
+  tmp1 <- mvtnorm::rmvnorm(n, mean=c(0, 0), sigma=matrix(c(1, rho, rho, 1), nrow=2))
+  mean(qnorm((1/(n+1)) * rank(tmp1[, 1]))   * qnorm((1/(n+1)) * rank(tmp1[, 2]))  )
+}
+func2 <- function(rho, B, func1, n){
+  results <- future_map_dbl(
+    1:B,
+    \(idx) func1(rho = rho, n = n), # Only 'idx' is iterated
+    .options = furrr_options(seed = 123)
+  )
+  mean(results)
+}
+Grho <- future_map_dbl(RHOs, func2, B = B, func1 = func1, n = 50, .options = furrr_options(seed = 123))
+
 # plot(Grho)
 # abline(0, 1)
-# plan(sequential)
+plan(sequential)
+
+# 
+# Fsfit <- vector("double", RUNs)
+# FTfit <- vector("double", RUNs)
+# FTfit1 <- vector("double", RUNs)
+# for(run in 1:RUNs){
+#   
+#   print(run)
+#   data <- Model(n, run * 123456, dimen)
+#   x <- data[[1]]$x
+#   vax <- data[[1]]$vax
+#   Sigma <- data[[2]]
+#   Omega <- data[[3]]
+#   Tx <- NSEstimator2(x, dimen)
+#   Tvax <- NSEstimator2(vax, dimen)
+#   
+#   
+#   
+#   # proper candidates of tuning parameters
+#   lamseq <- seq(1.5e-8, 0.15, length.out = 300)
+#   # lamseq <- seq(0.00182, 0.001824, length.out = 100)
+#   lambda.list <- list() # a list containing candidates of tuning parameters for each mode 
+#   for (i in 1:K) {
+#     lambda.list[[i]] <- lamseq
+#   }
+#   
+#   sfit <- Separate.fit.correct(x, vax, lambda.list = lambda.list)
+#   Tfit <- Separate.fit.correct(Tx, Tvax, lambda.list = lambda.list)
+#   Tfit1 <- Separate.fit.correct(Tx, Tvax, lambda.list = lambda.list, Grho=Grho)
+#   
+#   Fsfit[[run]] <- norm(sfit[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
+#   FTfit[[run]] <- norm(Tfit[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
+#   FTfit1[[run]] <- norm(Tfit1[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
+# }
+
+
 
 
 Fsfit <- vector("double", RUNs)
@@ -69,6 +105,7 @@ FTfit <- vector("double", RUNs)
 FTfit1 <- vector("double", RUNs)
 for(run in 1:RUNs){
   
+  print(run)
   data <- Model(n, run * 123456, dimen)
   x <- data[[1]]$x
   vax <- data[[1]]$vax
@@ -87,19 +124,37 @@ for(run in 1:RUNs){
     lambda.list[[i]] <- lamseq
   }
   
-  sfit <- Separate.fit.correct(x, vax, lambda.list = lambda.list)
-  Tfit <- Separate.fit.correct(Tx, Tvax, lambda.list = lambda.list)
-  Tfit1 <- Separate.fit.correct(Tx, Tvax, lambda.list = lambda.list, Grho=Grho)
+  xtilde.omega <- tilde.omega(x, dimen, n)
+  # xtilde.omega[[2]] <- diag(dimen[[2]])
+  xtilde_Sk <- tilde.sigma(x, xtilde.omega, dimen, 1, n)
+  Fsfit[[run]] <- norm(xtilde_Sk - Sigma[[1]], type="F")
   
-  Fsfit[[run]] <- norm(sfit[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
-  FTfit[[run]] <- norm(Tfit[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
-  FTfit1[[run]] <- norm(Tfit1[["Omegahat"]][[1]][[2]] - Sigma[[1]], type="F")
+  
+  Txtilde.omega <- tilde.omega(Tx, dimen, n)
+  # Txtilde.omega[[2]] <- diag(dimen[[2]])
+  Txtilde_Sk <- tilde.sigma(Tx, Txtilde.omega, dimen, 1, n)
+  FTfit[[run]] <- norm(Txtilde_Sk - Sigma[[1]], type="F")
+  
+  Tx1tilde.omega <- tilde.omega(Tx, dimen, n)
+  # Tx1tilde.omega[[2]] <- diag(dimen[[2]])
+  Tx1tilde_Sk <- tilde.sigma(Tx, Tx1tilde.omega, dimen, 1, n)
+  RHOs <- seq(-1, 1, 0.01)
+  for(i in 1:dimen[[1]]){
+    for(j in 1:dimen[[1]]){
+      tmp1 <- abs(Tx1tilde_Sk[i, j] - Grho)
+      Tx1tilde_Sk[i, j] <- RHOs[[which.min(tmp1)]]
+    }
+  }
+  FTfit1[[run]] <- norm(Tx1tilde_Sk - Sigma[[1]], type="F")
+  
+  
+
 }
 
 
-cat("Mean Forb. norm difference of sample Est. and True Sigma for p1:", mean(Fsfit), "With Sample size of", n)
-cat("Mean Forb. norm difference of transformed sample Est. and True Sigma for p1:", mean(FTfit), "With Sample size of", n)
-cat("Mean Forb. norm difference of transformed and corrected Est. and True Sigma for p1:", mean(FTfit1), "With Sample size of", n)
+cat("Mean Forb. norm diff. of raw sample Est. and True Sigma for p1:", mean(Fsfit),  "And sd:", sd(Fsfit), "With Sample size of", n, "\n\n")
+cat("Mean Forb. norm diff. of transformed sample Est. and True Sigma for p1:", mean(FTfit),   "And sd:", sd(FTfit),"With Sample size of", n, "\n\n")
+cat("Mean Forb. norm diff. of transformed and corrected Est. and True Sigma for p1:", mean(FTfit1),   "And sd:", sd(FTfit1),"With Sample size of", n, "\n\n")
 
 
 
