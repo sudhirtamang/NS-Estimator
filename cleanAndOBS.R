9df22374a9fec8e857db4d29e9196841692789e5
Sudhir Tamang <sudhirtamang5@gmail.com>
2026-07-06 12:47:53 -0400

    Added a New file

--- cleanAndOBS.R
+++ cleanAndOBS.R
@@ -7,3 +7,56 @@
 # DATE:     6th July, 2026 
 # ==============================================================================
 # 
+
+rm(list = ls())
+library(Tlasso)
+library(tensr)
+library(glasso)
+library(expm)
+library(rTensor)
+library(doParallel)
+library(furrr)
+
+
+
+source("simulation.summary.R")
+source("Lfunctions.R")
+source("Separate.fit.correct.R")
+source("Model.R")
+
+
+
+# RUNs <- 100
+RUNs <- 3
+n <- 50
+dimen <- c(45, 54)
+# dimen <- c(30, 36, 30)
+nvars <- prod(dimen)
+# dimen <- c(110, 4)
+
+K <- length(dimen)
+tensor.order <- length(dimen)
+
+
+d <- 1
+
+diff.f.sigma <- array(0, dim = c(RUNs, d)) # estimation error in Frobenius norm for each mode
+diff.f.omega <- array(0, dim = c(RUNs, d)) # estimation error in Frobenius norm for each mode
+diff.f.sigma.max <- array(0, dim = c(RUNs, d)) # estimation error in Frobenius norm for each mode
+diff.f.omega.max <- array(0, dim = c(RUNs, d)) # estimation error in Frobenius norm for each mode
+
+
+
+
+for(itr in 1:RUNs){
+  print(itr)
+  data <- Model(n, itr * 123456, dimen)
+  x <- data[[1]]$x
+  vax <- data[[1]]$vax
+  Sigma <- data[[2]]
+  Omega <- data[[3]]
+}
+
+
+
+
